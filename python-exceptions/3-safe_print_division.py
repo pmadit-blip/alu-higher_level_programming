fb75cfbfe41a77641913ab6be2872dc7d07483b3
pmadit-blip <p.madit@alustudent.com>
2026-07-09 11:31:31 -0700

    Add safe_print_division function

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result."""
+    result = None
+    try:
+        result = a / b
+        return result
+    except Exception:
+        return None
+    finally:
+        print("Inside result: {}".format(result))
