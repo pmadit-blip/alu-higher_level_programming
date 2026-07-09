b3933855cf5b11407a4bd2426e953e4e3ebb41bb
pmadit-blip <p.madit@alustudent.com>
2026-07-09 11:09:28 -0700

    Fix safe_print_list function

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
+
     for i in range(x):
         try:
-<<<<<<< HEAD
-            print("{}".format(my_list[i]), end="")
-=======
             print(my_list[i], end="")
->>>>>>> Add safe_print_list function
             count += 1
         except IndexError:
             break
+
     print()
     return count
