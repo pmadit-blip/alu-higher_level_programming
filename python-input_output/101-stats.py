ec17c20b8c042ad2e602cbbd142e890d5873cfeb
pmadit-blip <p.madit@alustudent.com>
2026-07-23 14:15:21 -0700

    Fix log parser output

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,52 +1,51 @@
 #!/usr/bin/python3
-"""Log statistics script."""
-
 import sys
 
-
-def print_stats(total_size, status_codes):
-    """Print statistics."""
-    print("File size: {}".format(total_size))
-
-    for code in sorted(status_codes):
-        if status_codes[code] != 0:
-            print("{}: {}".format(code, status_codes[code]))
-
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 
 total_size = 0
 line_count = 0
 
-status_codes = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+
+def print_stats():
+    """Print accumulated statistics."""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code]:
+            print("{}: {}".format(code, status_codes[code]))
+
 
 try:
     for line in sys.stdin:
-        parts = line.split()
-
         try:
-            status = int(parts[-2])
+            parts = line.split()
+
             size = int(parts[-1])
+            status = parts[-2]
 
             total_size += size
 
             if status in status_codes:
                 status_codes[status] += 1
 
+            line_count += 1
+
+            if line_count % 10 == 0:
+                print_stats()
+
         except (ValueError, IndexError):
-            pass
-
-        line_count += 1
-
-        if line_count % 10 == 0:
-            print_stats(total_size, status_codes)
+            continue
 
 except KeyboardInterrupt:
-    print_stats(total_size, status_codes)
+    pass
+finally:
+    print_stats()
