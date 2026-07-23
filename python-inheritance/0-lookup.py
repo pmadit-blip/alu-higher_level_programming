eab997b62c23df0fcd7700b855044512e9e1cbc2
pmadit-blip <p.madit@alustudent.com>
2026-07-23 01:21:04 -0700

    Document lookup function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module for looking up object attributes."""
+"""Module for looking up attributes and methods of an object."""
 
 
 def lookup(obj):
-    """Return a list of available attributes and methods of an object."""
+    """Return a list of attributes and methods available in an object."""
     return dir(obj)
