7d83d65dc52aefe486c3e8c6540850fd218d371a
pmadit-blip <p.madit@alustudent.com>
2026-07-23 01:15:09 -0700

    Add documentation to lookup function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module for looking up object attributes."""
+
+
 def lookup(obj):
+    """Return a list of available attributes and methods of an object."""
     return dir(obj)
