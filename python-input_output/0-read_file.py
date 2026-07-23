de1205947ff96178d53d57e38ee387c0b92d0c9f
pmadit-blip <p.madit@alustudent.com>
2026-07-23 04:32:36 -0700

    Add read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module for reading a text file."""
+"""Defines a function that reads a UTF-8 text file and prints it."""
 
 
 def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its contents to stdout."""
-    with open(filename, mode="r", encoding="utf-8") as f:
+    """Read a UTF-8 text file and print its contents."""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
