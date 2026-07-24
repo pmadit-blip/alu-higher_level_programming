46d06b0f3a49d303fded1cedff4c4cf4cd40773a
pmadit-blip <p.madit@alustudent.com>
2026-07-24 15:17:52 -0700

    Convert database to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- Converts database, table, and field to UTF8
+-- Convert database, table, and field to UTF8
 
 ALTER DATABASE hbtn_0c_0
 CHARACTER SET utf8mb4
@@ -8,8 +8,4 @@ USE hbtn_0c_0;
 
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE first_table
-MODIFY name VARCHAR(256)
 COLLATE utf8mb4_unicode_ci;
