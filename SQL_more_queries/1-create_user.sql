28a37854d0445b0551eccb8632f46a3fc4e5111d
pmadit-blip <p.madit@alustudent.com>
2026-07-26 02:45:04 -0700

    Add SQL script to create user_0d_1 with all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
--- Create MySQL user user_0d_1 with all privileges
-
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-
+-- Script that creates the MySQL user user_0d_1 with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
