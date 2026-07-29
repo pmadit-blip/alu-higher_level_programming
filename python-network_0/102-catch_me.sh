da1ff5c5e589ddd2b1194e5bade492f957e74fbc
Priscilla Yar Mabior <p.madit@alustudent.com>
2026-07-29 21:54:49 +0000

    Fix 102-catch_me.sh with correct header name

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to catch_me and displays You got me!
-curl -s -L -X PUT -H "Content-Type: application/json" -H "X-School-User-Id: 98" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -H "Content-Type: application/json" -H "X-HolbertonSchool-User-Id: 98" 0.0.0.0:5000/catch_me
