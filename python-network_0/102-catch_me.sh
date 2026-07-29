04eac1af954306c4106b9cb2d4c672a142578d93
Priscilla Yar Mabior <p.madit@alustudent.com>
2026-07-29 21:44:32 +0000

    Add bash script to catch the catch_me endpoint

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that makes a request to catch_me endpoint and displays You got me!
+# Script that makes a request to catch_me and displays You got me!
 curl -s -X PUT -H "Content-Type: application/json" 0.0.0.0:5000/catch_me
