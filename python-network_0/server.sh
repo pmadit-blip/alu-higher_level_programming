59c0921ec98881c62ab63f7e4ce42ce544d4cbd4
Priscilla Yar Mabior <p.madit@alustudent.com>
2026-07-29 22:58:32 +0000

    Fix 102-catch_me.sh try PUT method

---
+++ python-network_0/server.sh
@@ -0,0 +1,29 @@
+msg - [Got]
+<!doctype html>
+<html lang=en>
+<title>405 Method Not Allowed</title>
+<h1>Method Not Allowed</h1>
+<p>The method is not allowed for the requested URL.</p>
+
+(153 chars long)
+[stderr]: 
+(0 chars long)
+[Expected]
+You find me!
+(12 chars long)
+[stderr]: [Anything]
+(0 chars long)msg - [Got]
+<!doctype html>
+<html lang=en>
+<title>405 Method Not Allowed</title>
+<h1>Method Not Allowed</h1>
+<p>The method is not allowed for the requested URL.</p>
+
+(153 chars long)
+[stderr]: 
+(0 chars long)
+[Expected]
+You find me!
+(12 chars long)
+[stderr]: [Anything]
+(0 chars long)
