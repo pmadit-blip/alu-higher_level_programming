c9d742029ec67c5e70dfdaa8a4d36f6071c373a5
Priscilla Yar Mabior <p.madit@alustudent.com>
2026-07-29 21:46:26 +0000

    Fix 102-catch_me.sh to follow redirect with -L flag

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to catch_me and displays You got me!
-curl -s -X PUT -H "Content-Type: application/json" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -H "Content-Type: application/json" 0.0.0.0:5000/catch_me
