3deee33fcdb87d97f9d99c4f31f622542e511f92
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-07-30 05:48:44 +0000

    Fix module docstring in 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
+"""Script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))#!/usr/bin/python3
-import urllib.request
 
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     body = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(body)))
