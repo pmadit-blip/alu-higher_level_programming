77118c596e2339ae89b0d30999b0e3801333910a
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-07-30 05:55:11 +0000

    Fix module docstring in 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Script that takes a URL and displays the value of X-Request-Id header"""
+"""
+Script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable
+found in the header of the response.
+"""
 import urllib.request
 import sys
 
