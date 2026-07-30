772d51dd4b629fe8e813bc0b38617f0bc216ac17
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-07-30 06:43:35 +0000

    Fix docstring and PEP8 in 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module that fetches https://alu-intranet.hbtn.io/status using requests."""
+"""Module that fetches https://intranet.hbtn.io/status using requests."""
 import requests
 
 
-response = requests.get('https://alu-intranet.hbtn.io/status')
+r = requests.get('https://intranet.hbtn.io/status')
 print("Body response:")
-print("	- type: {}".format(type(response.text)))
-print("	- content: {}".format(response.text))
+print("	- type: {}".format(type(r.text)))
+print("	- content: {}".format(r.text))
