b2c1cee00c96ede8f36d538a4415ecea4f63c6c2
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-07-30 06:39:39 +0000

    Add 3-error_code.py fresh start

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Module that fetches a URL and handles HTTP error codes."""
+"""Module that sends a request to a URL and handles HTTP errors."""
 import urllib.request
 import urllib.error
 import sys
 
 
-url = sys.argv[1]
 try:
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.read().decode('utf-8'))
 except urllib.error.HTTPError as e:
     print("Error code: {}".format(e.code))
