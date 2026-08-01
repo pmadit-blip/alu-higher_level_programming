e3b8aceb8036c5803df390f404ac6e1febeadfb7
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-01 22:32:59 +0000

    Add cities states list relationship script

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Lists all City objects and their corresponding State"""
+"""Lists all City objects and their corresponding State."""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from relationship_state import Base, State
 from relationship_city import City
-from relationship_state import Base
 
 
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
         ),
         pool_pre_ping=True
     )
@@ -26,3 +28,5 @@ if __name__ == "__main__":
             city.name,
             city.state.name
         ))
+
+    session.close()
