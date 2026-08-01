3656c4e3ed4446f79402eba2b29db96d4f4f5786
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-01 22:15:45 +0000

    Add SQLAlchemy relationship between states and cities

---
+++ python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Relationship City model"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+
+
+class City(Base):
+    """City class"""
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+    state = relationship("State", back_populates="cities")#!/usr/bin/python3
+"""Relationship City model"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from relationship_state import Base
+
+
+class City(Base):
+    """City class"""
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+
+    state = relationship("State", back_populates="cities")
