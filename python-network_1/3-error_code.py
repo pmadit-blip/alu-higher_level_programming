b3bc7a4bb461e2a9c91568c7b8fba0204ea49ffb
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-01 08:34:49 +0000

    Add Python script to fetch URL and handle HTTP errors

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Module that sends a request to a URL and handles HTTP errors."""
+"""Script that sends a request and handles HTTP errors"""
 import urllib.request
 import urllib.error
 import sys
-
-
 try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(sys.argv[1]) as r:
+        print(r.read().decode("utf-8"))
 except urllib.error.HTTPError as e:
     print("Error code: {}".format(e.code))
