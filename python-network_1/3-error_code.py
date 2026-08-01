ac208031aa42eb176b05935dd32568d6b950d4cd
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-01 09:14:02 +0000

    Update docstring in 3-error_code.py

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that sends a request to a URL and displays the response body."""
+"""Sends a request to a URL and displays the body of the response."""
 import urllib.request
 import urllib.error
 import sys
