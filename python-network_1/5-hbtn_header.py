1adac0311c89855b8120614d0b654123ab153be8
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-05 18:32:27 +0000

    Fix blank lines in 5-hbtn_header.py

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Module that sends a request to a URL and displays X-Request-Id header."""
 import requests
-import sys
-
-
+import sys:
 r = requests.get(sys.argv[1])
 print(r.headers.get('X-Request-Id'))
