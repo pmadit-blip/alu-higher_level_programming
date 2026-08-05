24aeec0a1208e0b9e4b96ea84f5bd0b0e57b23f6
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-05 18:18:23 +0000

    Rewrite 3-error_code.py from scratch

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Module that sends a request to a URL and handles HTTP errors."""
+"""Module that fetches a URL and handles HTTP error codes."""
 import urllib.request
 import urllib.error
 import sys
 
 
 try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.read().decode('utf-8'))
+    with urllib.request.urlopen(sys.argv[1]) as r:
+        print(r.read().decode('utf-8'))
 except urllib.error.HTTPError as e:
     print("Error code: {}".format(e.code))
