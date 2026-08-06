04c7c1cfd4c9ee9f529ce082d80f9c08fce6bc30
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 12:07:10 +0000

    Add this file to the github

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""8-json_api"""
+"""Sends a letter to a search API and displays JSON result."""
 import requests
 import sys
 
 
-q = sys.argv[1] if len(sys.argv) > 1 else ""
-r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
-try:
-    j = r.json()
-    if j:
-        print("[{}] {}".format(j.get("id"), j.get("name")))
-    else:
-        print("No result")
-except ValueError:
-    print("Not a valid JSON")
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    try:
+        data = r.json()
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
