3e3048ad6a95d0ba8e936fd94e0e40bc6887ee92
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 11:01:42 +0000

    Fix docstring and PEP8 in 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-Script that takes GitHub credentials and displays the user id.
-Uses the GitHub API with Basic Authentication.
-"""
+"""Script that takes GitHub credentials and displays the user id."""
 import requests
 import sys
 
 url = "https://api.github.com/user"
 response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
 print(response.json().get("id"))
-
