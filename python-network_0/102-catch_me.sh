71fafe3dbbc2a7e878e2e2f6801577bfc0edd8e2
Priscilla Yar Mabior <p.madit@alustudent.com>
2026-08-06 13:25:56 +0000

    Fix catch me script

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
-1  #!/bin/bash
-2  # Sends a PUT request to the catch_me endpoint
-3  curl -sL -X PUT -H "X-School-User-Id: 98" 0.0.0.0:5000/catch_me
+#!/bin/bash
+# Sends a request to the catch_me endpoint
+curl -sL -X PUT -H "X-School-User-Id: 98" 0.0.0.0:5000/catch_me#!/bin/bash
