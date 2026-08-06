e8712d3a867c9d057240205419c8313a6a01b270
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 12:28:49 +0000

    Add GitHub commits script

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Script that lists 10 commits of a GitHub repository."""
+"""Lists the 10 most recent commits of a GitHub repository."""
+
 import requests
 import sys
 
 
-repo = sys.argv[1]
-owner = sys.argv[2]
-url = "https://api.github.com/repos/{}/{}/commits".format(
-    owner, repo)
-r = requests.get(url, params={"per_page": 10})
-commits = r.json()
-for commit in commits:
-    sha = commit.get("sha")
-    name = commit.get("commit").get("author").get("name")
-    print("{}: {}".format(sha, name))
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    response = requests.get(url)
+
+    for commit in response.json()[:10]:
+        print("{}: {}".format(
+            commit.get("sha"),
+            commit.get("commit").get("author").get("name")
+        ))
