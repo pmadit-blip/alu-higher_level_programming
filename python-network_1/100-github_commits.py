bb3f89b8ef30d1d4e5c245112fe04b5623c48ee2
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 12:13:25 +0000

    Add the file 100-github commits

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Module that lists 10 commits of a GitHub repository."""
+"""Uses GitHub API to display user id with Basic Auth."""
 import requests
 import sys
 
 
-repo = sys.argv[1]
-owner = sys.argv[2]
-url = "https://api.github.com/repos/{}/{}/commits".format(
-    owner, repo)
-r = requests.get(url, params={"per_page": 10})
-for commit in r.json():
-    sha = commit.get("sha")
-    name = commit.get("commit").get("author").get("name")
-    print("{}: {}".format(sha, name).encode(
-        "utf-8").decode("utf-8"))
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    r = requests.get(
+        "https://api.github.com/user",
+        auth=(username, token)
+    )
+    print(r.json().get("id"))
