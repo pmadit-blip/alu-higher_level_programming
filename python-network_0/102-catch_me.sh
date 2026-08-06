9af92cb1e243bccd34b998427ed905a71b9780dc
Priscilla Yar Mabior <p.madit@alustudent.com>
2026-08-06 13:34:32 +0000

    Fix catch me request headers

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request that makes the server respond with You got me!
-curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+# Sends a request that makes the server respond with You find me!
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
