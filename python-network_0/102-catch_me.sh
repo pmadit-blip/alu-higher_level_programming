efeba5aa5ba77fb7c30462cc9267962c22b5d9cd
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 12:42:06 +0000

    Fix catch me script

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
-# Networks in Python
-
-After surviving the brutality of the old version of mysql that intranet forced me to use and i ended up writting over 121 commits😭😭. Now here i am with python networks
+#!/bin/bash
+# Sends a request that makes the server respond with "You got me!"
+curl -sL -X PUT 0.0.0.0:5000/catch_me
