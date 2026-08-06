89e5b278b0021f0d2586341f21fcd6a3d5e2315e
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 12:19:33 +0000

    Fix argument order in 100-github_commits.py

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -8,9 +8,9 @@ repo = sys.argv[1]
 owner = sys.argv[2]
 url = "https://api.github.com/repos/{}/{}/commits".format(
     owner, repo)
-headers = {"Accept": "application/vnd.github.v3+json"}
-r = requests.get(url, params={"per_page": 10}, headers=headers)
-for commit in r.json():
+r = requests.get(url, params={"per_page": 10})
+commits = r.json()
+for commit in commits:
     sha = commit.get("sha")
     name = commit.get("commit").get("author").get("name")
     print("{}: {}".format(sha, name))
