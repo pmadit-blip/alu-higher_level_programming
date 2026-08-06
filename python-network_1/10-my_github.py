9c51be27d1612e007fd682c43cacc8b1f6ea8b17
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 10:59:07 +0000

    Fix docstring in 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Script that uses GitHub API to display user id."""
+"""
+Script that takes GitHub credentials and displays the user id.
+Uses the GitHub API with Basic Authentication.
+"""
 import requests
 import sys
 
 url = "https://api.github.com/user"
 response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
 print(response.json().get("id"))
+
