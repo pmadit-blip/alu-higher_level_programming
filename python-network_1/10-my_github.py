b8a2e708500b684b8227271f45999718da952d6b
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 11:08:29 +0000

    Rewrite 10-my_github.py cleanly

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Script that takes GitHub credentials and displays the user id."""
+"""Script that uses GitHub API to display user id."""
 import requests
 import sys
 
-url = "https://api.github.com/user"
-response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+response = requests.get("https://api.github.com/user",
+                        auth=(sys.argv[1], sys.argv[2]))
 print(response.json().get("id"))
+
