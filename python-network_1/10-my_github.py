581848ccf52c9b6897893515e0d06f33414145af
Priscilla Madit <YOUR_GITHUB_EMAIL>
2026-08-06 10:55:13 +0000

    Fix 10-my_github.py: remove duplicate code

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -5,11 +5,5 @@ import sys
 
 url = "https://api.github.com/user"
 response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
-print(response.json().get("id"))#!/usr/bin/python3
-"""Script that uses GitHub API to display user id."""
-import requests
-import sys
-
-url = "https://api.github.com/user"
-response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
 print(response.json().get("id"))
+
